b8e6996aa23c83abe2e7a73db2d4eafaac0ac535
Dan Dart <git@dandart.co.uk>
2025-08-14 17:08:47 +0100

    0.3.0.0

--- lib/Data/Model/User.hs
+++ lib/Data/Model/User.hs
@@ -3,27 +3,36 @@
 module Data.Model.User where
 
 import Data.Model
+import Data.Text               (Text)
 import Types.Email
 import Types.Name
 import Types.Password
 import Types.Username
-import Types.UserType
+-- import Types.UserType
 import Types.VerificationToken
 
 type MaybeVerificationToken  = Maybe VerificationToken
 
 modelUser ∷ Model
 modelUser = defaultModel {
-    modelName = "User",
-    pluralModelName = "Users",
-    endpoint = "user",
+    singularType = "User",
+    pluralType = "Users",
+    singularEndpoint = "user",
     pluralEndpoint = "users",
-    fields = [
-        defaultField { lowerField = "name", upperField = "Name", typeName = ''Name },
-        defaultField { lowerField = "username", upperField = "Username", typeName = ''Username },
-        defaultField { lowerField = "email", upperField = "Email", typeName = ''Email },
-        defaultField { lowerField = "password", upperField = "Password", typeName = ''Password },
-        defaultField { lowerField = "userType", upperField = "Type", typeName = ''UserType },
-        defaultField { lowerField = "verificationToken", upperField = "VerificationToken", typeName = ''MaybeVerificationToken }
-    ]
+    createFields = Nothing, -- only via register I guess - todo really?
+    retrieveFields = Just [
+        defaultField { lowerField = "username", upperField = "Username", dbField = "username", typeName = ''Username }, -- don't include sql field???
+        defaultField { lowerField = "email", upperField = "Email", dbField = "email", typeName = ''Email },
+        defaultField { lowerField = "fullName", upperField = "FullName", dbField = "full_name", typeName = ''Name },
+        defaultField { lowerField = "givenName", upperField = "GivenName", dbField = "given_name", typeName = ''Name },
+        defaultField { lowerField = "secretEmailId", upperField = "SecretEmailId", dbField = "secret_email_id", typeName = ''Text } -- retrieve only, random regenerate available
+        ],
+    updateFields = Just [
+        defaultField { lowerField = "username", upperField = "Username", dbField = "username", typeName = ''Username }, -- don't include sql field???
+        defaultField { lowerField = "email", upperField = "Email", dbField = "email", typeName = ''Email },
+        defaultField { lowerField = "fullName", upperField = "FullName", dbField = "full_name", typeName = ''Name },
+        defaultField { lowerField = "givenName", upperField = "GivenName", dbField = "given_name", typeName = ''Name },
+        defaultField { lowerField = "password", upperField = "Password", dbField = "password", typeName = ''Password } -- update only, clever huh
+        ],
+    deleteFields = Nothing -- not supported yet
 }
