df995324cd53748004984a74e99ed462cf135096
Dan Dart <git@dandart.co.uk>
2025-12-13 18:48:53 +0000

    givenName -> chosenName

--- lib/Data/Model/User.hs
+++ lib/Data/Model/User.hs
@@ -19,19 +19,19 @@ modelUser = defaultModel {
     pluralType = "Users",
     singularEndpoint = "user",
     pluralEndpoint = "users",
-    createFields = Nothing, -- only via register I guess - todo really?
+    createFields = Nothing, -- only via register I guess - todo really? Use this for invite?
     retrieveFields = Just [
         defaultField { lowerField = "username", upperField = "Username", dbField = "username", typeName = ''Username }, -- don't include sql field???
         defaultField { lowerField = "email", upperField = "Email", dbField = "email", typeName = ''Email },
         defaultField { lowerField = "fullName", upperField = "FullName", dbField = "full_name", typeName = ''Name },
-        defaultField { lowerField = "givenName", upperField = "GivenName", dbField = "given_name", typeName = ''Name },
-        defaultField { lowerField = "secretEmailId", upperField = "SecretEmailId", dbField = "secret_email_id", typeName = ''Text } -- retrieve only, random regenerate available
+        defaultField { lowerField = "chosenName", upperField = "ChosenName", dbField = "chosen_name", typeName = ''Name },
+        defaultField { lowerField = "secretEmailId", upperField = "SecretEmailId", dbField = "secret_email_id", typeName = ''Text, filterOptions = FilterEq } -- retrieve only, random regenerate available
         ],
     updateFields = Just [
         defaultField { lowerField = "username", upperField = "Username", dbField = "username", typeName = ''Username }, -- don't include sql field???
         defaultField { lowerField = "email", upperField = "Email", dbField = "email", typeName = ''Email },
         defaultField { lowerField = "fullName", upperField = "FullName", dbField = "full_name", typeName = ''Name },
-        defaultField { lowerField = "givenName", upperField = "GivenName", dbField = "given_name", typeName = ''Name },
+        defaultField { lowerField = "chosenName", upperField = "ChosenName", dbField = "chosen_name", typeName = ''Name },
         defaultField { lowerField = "password", upperField = "Password", dbField = "password", typeName = ''Password } -- update only, clever huh
         ],
     deleteFields = Nothing -- not supported yet
